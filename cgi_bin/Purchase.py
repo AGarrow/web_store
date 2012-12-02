3f33afc39e4c6385dc5defc4a2a69c7cfc071f5c
= <=>
2012-12-02 16:14:34 -0500

    getting ready to overhaul Purchase.py

--- cgi_bin/Purchase.py
+++ cgi_bin/Purchase.py
@@ -7,14 +7,20 @@ def checkAccount():
 	fn= os.path.abspath('..')+'/databases/Inventory.csv'
 	catalogue = open(fn,'r')
 	form = cgi.FieldStorage()
+	if form.getvalue('light-purchase'):
+		print "ordered a lightweight!"
+	if form.getvalue('mid-purchase'):
+		print "ordered a mid!"
 	if form.has_key("username"):
 		self.username = form["username"].value
+		print "username:"+username
 		if form.has_key("password"):
 			self.password = form["password"].value
 
 		for line in catalogue.readlines():
 			entry = line.split(',')
-			if line[0] == username & line[1] == password:
+			if line[0] == username and line[1] == password:
+				print "matches!"	
 				return True
 	return False
 
@@ -24,10 +30,11 @@ def checkAccount():
 print "Content-type: text/html\n\n"
 print "<html>\n"
 print"<h1>it works! </h1>"
-if checkAccount():
+if checkAccount() == True:
 	print "hello "+username+"\n"
 else:
 	print "username and password do not match\n"
+	print "Location:../home.html"
 print "</html>\n"
 
 
