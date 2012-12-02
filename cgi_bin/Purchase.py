68d39163ebd9b7620ef4d02d35fa7df4d5cfe494
= <=>
2012-12-02 18:16:46 -0500

    fixed formatting

--- cgi_bin/Purchase.py
+++ cgi_bin/Purchase.py
@@ -1,33 +1,32 @@
 #!/usr/local/bin/python
 import cgi
 import os
-class Response:
+class Purchase:
 	def __init__(self):
 		self.username = ""
 		self.password = ""
 		self.light_quantity = ""
 		self.mid_quantity = ""
 		self.heavy_quantity = ""
-	def parseForm(self):
-		form = cgi.FieldStorage()
-		if form.has_key("username") and form["username"].value != "":
-			self.username = form["username"].value
+	def parseForm(self,form):
+		if form.has_key("username") and form["username"].value != "" :
+			self.username = form["username"].value.strip()
 		if form.has_key("password") and form["password"].value != "":
-			self.password = form["password"].value
+			self.password = form["password"].value.strip()
 		if form.getvalue("light-purchase"):
-			self.light_quantity = form["light-quantity"].value
+			self.light_quantity = form["light-quantity"].value.strip()
 		if form.getvalue("mid-purchase"):
-			self.mid_quantity = form["mid-quantity"].value
+			self.mid_quantity = form["mid-quantity"].value.strip()
 		if form.getvalue("heavy-purchase"):
-			self.heavy_quantity = form["heavy-quantity"].value
+			self.heavy_quantity = form["heavy-quantity"].value.strip()
 
 
 	def checkAccount(self):
-		fn= os.path.abspath('..')+'/databases/Inventory.csv'
+		fn= os.path.abspath('..')+'/databases/members.csv'
 		catalogue = open(fn,'r')
 		for line in catalogue.readlines():
 			entry = line.split(',')
-			if line[0] is self.username and line[1] is self.password:
+			if entry[0].strip() == self.username and entry[1].strip() == self.password:
 				return True
 		return False
 
@@ -43,8 +42,8 @@ class Response:
 		print "Content-type: text/html\n\n"
 		print "<html>\n"
 		print "<head>"
-		print """<link href = "./css/bootstrap.min.css" media="all" rel="stylesheet" type="text/css"/>"""
-		print """<link href = "./css/custom.css" media = "all" rel="stylesheet" type="text/css">"""
+		print """<link href = "../pages/css/bootstrap.min.css" media="all" rel="stylesheet" type="text/css"/>"""
+		print """<link href = "../pages/css/custom.css" media = "all" rel="stylesheet" type="text/css">"""
 		print "</head>"
 
 	def printMenu(self):
@@ -55,21 +54,23 @@ class Response:
 		print """<li><a href="../home.html">HOME<i class="icon-home"></i></a></li>"""
 		print "</ul>"
 		print """<ul class = "nav pull-right">"""
-		print """<li><a href="catalogue.html">Catalogue</a></li>"""
-		print """<li><a href="login.html" target="_blank">Login</a></li>"""
+		print """<li><a href="../pages/catalogue.html">Catalogue</a></li>"""
+		print """<li><a href="../pages/login.html" target="_blank">Login</a></li>"""
 		print "</ul>"
 		print "</nav>"
 		print "</div>"
 		print "</header>"
 
 	def printError(self):
-		print "error"
+		print """<div class = "row-fluid">"""
+		print "<p>error</p>"
+		print"</div>"
 
 	def printBill(self):
 		print "bill"
-
-order = Response()
-order.parseForm()
+form = cgi.FieldStorage()
+order = Purchase()
+order.parseForm(form)
 order.printHeader()
 print "<body>"
 order.printMenu()
@@ -80,7 +81,7 @@ if order.checkAccount():
 else:
 	order.printError()
 print "</body>"
-
+print "</html>"
 
 
 	
